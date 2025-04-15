3197427aaaba6d335ea603ba3ed9e877a70dcd6d
Leviron <nguyenphuoc4805@gmail.com>
2025-04-15 22:13:28 +0700

    enhance UI

--- app/components/what-we-do.tsx
+++ app/components/what-we-do.tsx
@@ -36,7 +36,7 @@ const WhatWeDo = () => {
                     </div>
                     <div className='flex-[3] mb-4 text-left'>
                         <h2 className='text-[18px] sm:text-3xl md:text-4xl lg:text-4xl font-bold  pb-4'>The <span className='text-purple-700'>problem</span> we&apos;re solving</h2>
-                        <p className='text-base sm:text-lg md:text-xl lg:text-[18px] text-justify'>Today&apos;s carbon tracking systems are fragmented, opaque, and often inaccurate. Scope 3 emissions—those embedded in supply chains—represent up to 80% of many companies' carbon footprints, yet they remain the most difficult to measure and verify. As regulations tighten and consumers demand greater transparency, businesses need better solutions.</p>
+                        <p className='text-base sm:text-lg md:text-xl lg:text-[18px] text-justify'>Today&apos;s carbon tracking systems are fragmented, opaque, and often inaccurate. Scope 3 emissions—those embedded in supply chains—represent up to 80% of many companies&apos; carbon footprints, yet they remain the most difficult to measure and verify. As regulations tighten and consumers demand greater transparency, businesses need better solutions.</p>
                     </div>
                 </div>
                 <div className='flex justify-between items-center w-full border-b-2 border-b-carbon gap-12'>
